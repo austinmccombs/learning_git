704542fa114f6f73c39552145ccb29eef12006e9
Austin McCombs <austinmccombs22@gmail.com>
2018-08-22 19:49:20 -0600

    committing for safety

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-document.addEventListener("DOMContentLoaded", function()){
+document.addEventListener("DOMContentLoaded", function(){
     //variables
+    var player
     var name = document.getElementById('name')
     var startButton = document.getElementById('start-button')
+    var top = document.getElementById('top')
     name.addEventListener('keyup', function(event){
         if (event.target.value !== '') {
         startButton.className=""
         } else {
-            startButton.className="hide"
+            startButton.className='hide'
         }
     })
 
 
     //functions
-}
+
+    function startGame() {
+        player = name.value
+        startButton.className = 'hide'
+        var label = document.createElement('p')
+        label.innerHTML = 'Welcome ' + player
+        label.className = 'center'
+        top.append(label)
+    }
+
+    startButton.addEventListener('cleck', startGame)
+})
